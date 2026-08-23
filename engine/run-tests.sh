4a77d6bf13c0f9fe63b1a1d69603e97e0278d4fa
locon007-creator <locon007@gmail.com>
2026-08-23 02:36:40 -0400

    test(engine): include project injection suite

--- engine/run-tests.sh
+++ engine/run-tests.sh
@@ -9,3 +9,4 @@ javac --release 17 -Xlint:all -Werror -d "$OUT" @"$ROOT/.sources"
 rm -f "$ROOT/.sources"
 java -cp "$OUT" com.osulsa.apkbuilder.engine.EngineSelfTest
 java -cp "$OUT" com.osulsa.apkbuilder.engine.ProjectArchiveSelfTest
+java -cp "$OUT" com.osulsa.apkbuilder.engine.ApkProjectInjectorSelfTest
