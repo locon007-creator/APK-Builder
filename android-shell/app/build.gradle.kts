3dd48c604f03e6e047c5893cceef3b1fcf7dff34
locon007-creator <locon007@gmail.com>
2026-08-24 15:40:16 -0400

    Add configurable Android application module

---
+++ android-shell/app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+}
+
+val generatedAppId = providers.gradleProperty("APP_ID").orElse("com.apkbuilder.generated")
+val generatedAppName = providers.gradleProperty("APP_NAME").orElse("Generated App")
+val generatedVersionName = providers.gradleProperty("VERSION_NAME").orElse("1.0")
+val generatedVersionCode = providers.gradleProperty("VERSION_CODE").orElse("1")
+
+android {
+    namespace = "com.apkbuilder.shell"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = generatedAppId.get()
+        minSdk = 26
+        targetSdk = 36
+        versionCode = generatedVersionCode.get().toInt()
+        versionName = generatedVersionName.get()
+        manifestPlaceholders["appLabel"] = generatedAppName.get()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
